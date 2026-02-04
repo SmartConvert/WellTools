e85c790ed9ea63d26bb122bbb9950b673a20ffff
SmartConvert <businessmailus2@gmail.com>
2026-02-04 22:37:52 +0100

    change

--- scripts/update-sitemap.js
+++ scripts/update-sitemap.js
@@ -34,8 +34,7 @@ async function updateSitemap() {
         ];
 
         let sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 `;
 
         // Add Static URLs

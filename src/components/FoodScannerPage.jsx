f88473b303be1eeb6d83005283c3b8f7993085a5
SmartConvert <businessmailus2@gmail.com>
2026-03-25 22:13:46 +0100

    Fix navbar overlap on FoodScannerPage

--- src/components/FoodScannerPage.jsx
+++ src/components/FoodScannerPage.jsx
@@ -77,7 +77,7 @@ function FoodScannerPage() {
     };
 
     return (
-        <div className="min-h-[80vh] flex flex-col justify-center bg-slate-900 text-white font-sans py-12 px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen flex flex-col justify-center bg-slate-900 text-white font-sans pt-28 pb-16 px-4 sm:px-6 lg:px-8">
             <div className="max-w-4xl w-full mx-auto space-y-8">
                 {/* Header */}
                 <div className="text-center">
